f10856e1a8c319d0a2a72e0686d6618837b115eb
Jonus Li <jonus225@uw.edu>
2024-07-30 11:02:35 -0700

    chore: Added two router paths in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import WineListPage from "./components/WineListPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <WineListPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/wine-list/:id" element={<WineListPage />}></Route>
+        <Route path="/" element={<h1>BRIAN</h1>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
